9671826da3aa4c74a643f6efad0733bfba2f06ba
praj25 <sail@IAZI.LOCAL>
2017-08-14 15:07:53 +0530

    Added condition in home activity

--- app/src/main/java/com/example/karmali/homexperts/HomeActivity.java
+++ app/src/main/java/com/example/karmali/homexperts/HomeActivity.java
@@ -1,10 +1,13 @@
 package com.example.karmali.homexperts;
 
+import android.*;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
 
@@ -30,9 +33,26 @@ public class HomeActivity extends AppCompatActivity {
        String deviceId =  Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
        if(deviceId.equals("84d2789836089ebf"))
         {
-            Intent intent = new Intent(HomeActivity.this, ShowLocationSettingActivity.class);
-            startActivity(intent);
-            finish();
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (ActivityCompat.checkSelfPermission(HomeActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+                    {
+                        final Intent mainIntent = new Intent(HomeActivity.this, UserLocationActivity.class);
+                        startActivity(mainIntent);
+                        HomeActivity.this.finish();
+                    }
+                    else
+                    {
+                        final Intent mainIntent = new Intent(HomeActivity.this, ShowLocationSettingActivity.class);
+                        startActivity(mainIntent);
+                        HomeActivity.this.finish();
+                    }
+
+                }
+            }, 5000);
+
         }
         else
         {
